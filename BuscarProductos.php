2cc6a52eaaf21b10a395a7b8d431f53b53853000
NotebookRC <NotebookRC@NotebookRC-PC.mshome.net>
2014-05-27 21:37:09 -0400

    Agregué la cabecera stantard de xhtml a todas las páginas.

--- BuscarProductos.php
+++ BuscarProductos.php
@@ -1,9 +1,8 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
-<!DOCTYPE html>
-<html>
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
+<!--BuscarProductos.php -->  
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
--- Index.php
+++ Index.php
@@ -1,6 +1,8 @@
-<!doctype html>
-<!--index.php -->
-<html lang="es">
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
+<!--index.php -->  
 <head>
   <meta charset="UTF-8">
     <title></title>
