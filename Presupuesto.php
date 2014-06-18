68d3add64258586a7a7c3b34069cb6a68e8fd9d6
NotebookRC <NotebookRC@NotebookRC-PC.mshome.net>
2014-06-18 01:15:55 -0400

    Quito en Page Header en cotizacion ya que separa mucho la página

--- Presupuesto.php
+++ Presupuesto.php
@@ -1,9 +1,7 @@
 <!-- presupuesto -->
 <div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        <div class="page-header">
-            <h1>Generar Presupuesto</h1>
-        </div>
+    <div class="col-xs-12">
+            <h3>Generar Presupuesto</h3>
     </div>   
 </div>
 
