158322db735eb1cbfb0e388dc09e4213a0b2bbb7
obrared2014 <obraredchile@gmail.com>
2014-06-30 10:51:36 -0400

    Update Contacto.php
    Prueba de Pull

--- Contacto.php
+++ Contacto.php
@@ -1,4 +1,4 @@
-<!--Contacto.php -->
+<!--Contacto.php-->
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <div class="page-header">
                 <h1>Contactate con nosotros</h1>
